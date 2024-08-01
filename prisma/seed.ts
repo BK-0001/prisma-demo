11ef9928906c9d2839b5be91268bed888eac4cf6
Bart Kang <bart.kang.0719@gmail.com>
2024-08-01 12:04:47 -0700

    seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,29 +4,25 @@ const prisma = new PrismaClient();
 
 const run = async () => {
   const account = await prisma.account.create({
-    data: {
-      email: "bart5@gmail.com",
-      profile: {
-        create: {
-          boards: {
-            createMany: {
-              data: [{ name: "board1" }, { name: "board2" }]
-            }
-          }
-        }
-      }
-    },
-    include: {
-      profile: {
-        include: {
-          boards: true
-        }
-      }
-    }
+    data: { email: "bart@gmail.com" }
   });
 
-  console.log(JSON.stringify(account));
-  console.log("generated");
+  console.log(account);
+
+  const profile = await prisma.profile.create({
+    data: { firstName: "bart", accountId: account.id }
+  });
+
+  console.log(profile);
+
+  const boards = await prisma.board.createManyAndReturn({
+    data: [
+      { name: "Board 1", profileId: profile.id },
+      { name: "my secret board", isPublic: false, profileId: profile.id }
+    ]
+  });
+
+  console.log(boards);
 };
 
 run();
