56261b0654315d04d4cc7d6c27f7cdc54b421627
Bart Kang <bart.kang.0719@gmail.com>
2024-08-01 11:47:29 -0700

    add seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,32 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+const run = async () => {
+  const account = await prisma.account.create({
+    data: {
+      email: "bart5@gmail.com",
+      profile: {
+        create: {
+          boards: {
+            createMany: {
+              data: [{ name: "board1" }, { name: "board2" }]
+            }
+          }
+        }
+      }
+    },
+    include: {
+      profile: {
+        include: {
+          boards: true
+        }
+      }
+    }
+  });
+
+  console.log(JSON.stringify(account));
+  console.log("generated");
+};
+
+run();
