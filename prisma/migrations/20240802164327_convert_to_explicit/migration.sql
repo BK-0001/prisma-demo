9ed5d4300a0eb82d2e6e9960948f1c45cf1fd562
Bart Kang <bart.kang.0719@gmail.com>
2024-08-02 12:54:29 -0700

    fix migration file

--- prisma/migrations/20240802164327_convert_to_explicit/migration.sql
+++ prisma/migrations/20240802164327_convert_to_explicit/migration.sql
@@ -30,28 +30,3 @@ ALTER TABLE "BoardPin" ADD CONSTRAINT "BoardPin_boardId_fkey" FOREIGN KEY ("boar
 -- AddForeignKey
 ALTER TABLE "BoardPin" ADD CONSTRAINT "BoardPin_pinId_fkey" FOREIGN KEY ("pinId") REFERENCES "pins"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
-
-SELECT
-  json_build_object(
-    'id', p.id,
-    'name', p.name,
-    'image', p.image,
-    'createdAt', p.created_at,
-    'updatedAt', p.updated_at,
-    'boards', json_agg(
-      json_build_object(
-        'id', b.id,
-        'name', b.name,
-        'isPublic', b.is_public,
-        'createdAt', b.created_at,
-        'updatedAt', b.updated_at
-      )
-    )
-  ) as pin_with_boards
-FROM
-  pins p
-JOIN
-  board_pins bp ON p.id = bp.pin_id
-JOIN
-  boards b ON bp.board_id = b.id
-GROUP BY p.id;
